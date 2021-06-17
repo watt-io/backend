26bf895075cf71c7110727246b01d2589dd30134
Joao Koritar <jakoritarleite@gmail.com>
2021-06-17 12:51:55 -0300

    app.main: add CORS to the application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 import fastapi
+import starlette.middleware.cors
 
 from app.api.routes import router
 from app.events import create_start_app_handler
 
 def initialize_app() -> fastapi.FastAPI:
 	app = fastapi.FastAPI(title='WATTIO Coding Test', version="0.0.1")
+
+	app.add_middleware(
+		starlette.middleware.cors.CORSMiddleware,
+		allow_origins=['*'],
+		allow_credentials=True,
+		allow_methods=['GET', 'POST'],
+		allow_headers=['*']
+	)
 
 	app.add_event_handler('startup', create_start_app_handler())
 	app.include_router(router.routes)
