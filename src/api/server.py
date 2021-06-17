be271ab1feaa7c89fd192142d6f2e88692ccad76
JoaoVitorAmorim <almeidaamorimjoaovitor@gmail.com>
2021-06-17 20:21:03 -0300

    Successfully implemented POSTGRES Container

--- src/api/server.py
+++ src/api/server.py
@@ -1,10 +1,14 @@
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+print("File:server.py Before imports")
+from src.core import config,tasks
 from src.api.routes import router as api_router
 
+print("File:server.py After imports")
 def get_application():
-    app = FastAPI(title="Phresh", version="1.0.0")
+  
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -12,6 +16,9 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(api_router, prefix="/api")
     return app
+    
 app = get_application()
--- src/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-Base = declarative_base()
-class User(Base):
-    __tablename__ = "filmes"
-    id = Column(Integer, primary_key=True, index=True)
-    tittle = Column(String,)
