c61c2446385de9b764558b6748d28c3dc05d3f6b
Joao Koritar <jakoritarleite@gmail.com>
2021-06-17 15:19:32 -0300

    app.db.operations (filmes): create the CRUD operations as Database operations

---
+++ app/db/operations/filmes.py
@@ -0,0 +1,27 @@
+from .. import mongo
+
+# define the MongoDB Instance globally into the module
+mongo = mongo.dbLayer()
+
+# add a new movie into to the database
+async def add_movie(movie_data: dict) -> dict:
+	movie = await mongo.db.get_collection('movies').insert_one(movie_data)
+	inserted_movie = await mongo.db.get_collection('movies').find_one({'_id': movie.inserted_id}, projection={'_id': False})
+
+	return inserted_movie
+
+
+# retrieve all present movies in the Database
+async def retrieve_movies():
+	movies = []
+	async for movie in mongo.db.get_collection('movies').find(projection={'_id': False}):
+		movies.append(movie)
+
+	return movies
+
+
+# retrieve a movie from the database with a matching ID
+async def retrieve_movie(movie_id: str) -> dict:
+	movie = await mongo.db.get_collection('movies').find_one({'id': movie_id}, projection={'_id': False})
+	if movie:
+		return movie
