4cff4f02c311d0e558a83932db77d953c73fdbc9
Joao Koritar <jakoritarleite@gmail.com>
2021-06-17 15:16:17 -0300

    app.db (mongo): use motor instead of pymongo
    pymongo is a really good library
    
    but it doesn't let us use async functions within it

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,13 +1,18 @@
-import pymongo
+import motor.motor_asyncio
 
 class dbLayer(object):
 	def __init__(self):
 		self.__open_connection()
 
 	def __open_connection(self):
-		# Hardcoded database URL and PORT isn't a good practice but it's okay since we're using just two containers
+		# Hardcoded database URL and PORT isn't a good practice but it's okay since we're changing it soon
 		# mongodb://<username>:<password@<host>:<port>/<database>
 		mongo_uri = 'mongodb://adam:BADPRACTICEHEREFOLKS@172.16.238.10:27017'
+		mongo_connection_args = {
+			'zlibCompressionLevel': 7,
+			'compressors': 'zlib'
+		}
 
-		self.client = pymongo.MongoClient(mongo_uri)
-		self.db = self.client['backend']
+		self.client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri, **mongo_connection_args)
+		self.db = self.client.get_database('backend')
+		print(self.db.get_collection('movies'))
--- app/db/preload.py
+++ app/db/preload.py
@@ -1,13 +1,13 @@
-from .mongo import dbLayer
+from .operations.filmes import (
+	add_movie,
+	retrieve_movie
+)
 
-def load() -> None:
-	database = dbLayer()
-	if database.db['movies'].find_one({ "id": "8364" }):
+async def load() -> None:
+	if await retrieve_movie('8364'):
 		return
 
-	database.db['movies'].insert_many([
-		{ "id": "9207", "title": "The Week Of", "genre": "comedy", "release": "2018", "imdb": 5.2 },
-		{ "id": "6532", "title": "The Ridiculous 6", "genre": "action/comedy", "release": "2015", "imdb": 4.8 },
-		{ "id": "0c22", "title": "Sandy Wexler", "genre": "comedy", "release": "2017", "imdb": 5.2 },
-		{ "id": "8364", "title": "You Don't Mess with the Zohan", "genre": "comedy", "release": "2008", "imdb": 5.5 }
-	])
+	await add_movie({ "id": "9207", "title": "The Week Of", "genre": "comedy", "release": "2018", "imdb": 5.2 })
+	await add_movie({ "id": "6532", "title": "The Ridiculous 6", "genre": "action/comedy", "release": "2015", "imdb": 4.8 })
+	await add_movie({ "id": "0c22", "title": "Sandy Wexler", "genre": "comedy", "release": "2017", "imdb": 5.2 })
+	await add_movie({ "id": "8364", "title": "You Don't Mess with the Zohan", "genre": "comedy", "release": "2008", "imdb": 5.5 })
