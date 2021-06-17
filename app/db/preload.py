9da14c2433e97532b22230cd2ff81bec584bded5
Joao Koritar <jakoritarleite@gmail.com>
2021-06-17 12:56:26 -0300

    app.db (preload): change imdb type to float

--- app/db/preload.py
+++ app/db/preload.py
@@ -6,8 +6,8 @@ def load() -> None:
 		return
 
 	database.db['movies'].insert_many([
-		{ "id": "9207", "title": "The Week Of", "genre": "comedy", "release": "2018", "imdb": "5.2" },
-		{ "id": "6532", "title": "The Ridiculous 6", "genre": "action/comedy", "release": "2015", "imdb": "4.8" },
-		{ "id": "0c22", "title": "Sandy Wexler", "genre": "comedy", "release": "2017", "imdb": "5.2" },
-		{ "id": "8364", "title": "You Don't Mess with the Zohan", "genre": "comedy", "release": "2008", "imdb": "5.5" }
+		{ "id": "9207", "title": "The Week Of", "genre": "comedy", "release": "2018", "imdb": 5.2 },
+		{ "id": "6532", "title": "The Ridiculous 6", "genre": "action/comedy", "release": "2015", "imdb": 4.8 },
+		{ "id": "0c22", "title": "Sandy Wexler", "genre": "comedy", "release": "2017", "imdb": 5.2 },
+		{ "id": "8364", "title": "You Don't Mess with the Zohan", "genre": "comedy", "release": "2008", "imdb": 5.5 }
 	])
