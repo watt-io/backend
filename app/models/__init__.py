ebe5e8fe8a03dd06a4c036e5780dce5e2f002297
Joao Koritar <jakoritarleite@gmail.com>
2021-06-17 15:18:30 -0300

    app.models (movie): rewrite the models to be according with FastAPI

--- app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .movie import (
-	Movie
-)
--- app/models/movie.py
+++ app/models/movie.py
@@ -1,10 +1,25 @@
-import bson
-import pydantic
+from pydantic import (
+	BaseModel,
+	Field
+)
 
-class Movie(pydantic.BaseModel):
-	_id: bson.ObjectId
-	id: str
-	title: str
-	genre: str
-	release: str
-	imdb: float
+class MovieSchema(BaseModel):
+	id: str = Field(...)
+	title: str = Field(...)
+	genre: str = Field(...)
+	release: str = Field(...)
+	imdb: float = Field(..., gt=0.0, lt=10.0)
+
+def ResponseModel(data, message):
+	return {
+		"data": [data],
+		"code": 200,
+		"message": message
+	}
+
+def ErrorResponseModel(error, code, message):
+	return {
+		"error": error,
+		"code": code,
+		"message": message
+	}
