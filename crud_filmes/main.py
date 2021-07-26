7dbb1bf176a7700a66b89e10ad0cc17fc7e61bef
alyson <alysonhenrique26@gmail.com>
2021-07-26 02:16:49 -0300

    ADD persistencia inicial dos dados

--- crud_filmes/main.py
+++ crud_filmes/main.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
-app = FastAPI()
+app = FastAPI() 
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/filmes')
-def get_all_movies(request: schemas.Filme):
-    return request
+def insert_movie(request: schemas.Filme, db: Session = Depends(get_db)):
+    new_film = models.Filme(
+        nome=request.nome, 
+        ano=request.ano, 
+        categoria=request.categoria
+        )
+    db.add(new_film)
+    db.commit()
+    db.refresh(new_film)
+
+    return new_film
--- crud_filmes/schemas.py
+++ crud_filmes/schemas.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 
 class Filme(BaseModel):
-    id: int
     nome: str
     ano: int
     categoria: str
