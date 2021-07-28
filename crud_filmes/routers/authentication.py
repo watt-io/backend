be5ac56c54f97ad14424e5a7442ea586ba931194
alyson <alysonhenrique26@gmail.com>
2021-07-28 07:12:33 -0300

    Fim da autenticacao

--- crud_filmes/routers/authentication.py
+++ crud_filmes/routers/authentication.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from fastapi.param_functions import Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from starlette import status
 from .. import schemas, database, models, hashing, token
 from sqlalchemy.orm import Session
@@ -13,7 +14,7 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -23,6 +24,6 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         detail="Incorrect password")
     
     # gera um chave jwt e retorna
-    
+
     access_token = token.create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
--- crud_filmes/routers/filmes.py
+++ crud_filmes/routers/filmes.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from .. import schemas, database, models
+from fastapi import APIRouter, Depends, status
+from .. import schemas, database
 from typing import List
 from sqlalchemy.orm import Session
 from ..repository import filmes
+from .. import oauth2
 
 router = APIRouter(
     prefix="/filmes",
@@ -12,19 +13,28 @@ router = APIRouter(
 get_db = database.get_db
 
 @router.get('/', response_model=List[schemas.ShowAllFilme])
-def get_all_movies(db: Session = Depends(get_db)):
+def get_all_movies(db: Session = Depends(get_db), current_user: schemas.User = Depends(
+    oauth2.current_user
+)):
     return filmes.get_all(db)
 
 @router.post('/',  status_code=status.HTTP_201_CREATED,
             response_model=schemas.ShowFilme)
-def insert_movie(request: schemas.Filme, db: Session = Depends(get_db)):
+def insert_movie(request: schemas.Filme, db: Session = Depends(get_db),
+        current_user: schemas.User = Depends(
+    oauth2.current_user
+)):
     return filmes.insert(request, db)
 
 @router.get('/{id}', status_code=status.HTTP_200_OK,
             response_model=schemas.ShowFilme)
-def get_a_movie(id:int, db: Session = Depends(get_db)):
+def get_a_movie(id:int, db: Session = Depends(get_db), current_user: schemas.User = Depends(
+    oauth2.current_user
+)):
     return filmes.get(id, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_movie(id: int, db: Session = Depends(get_db)):
+def delete_movie(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(
+    oauth2.current_user
+)):
     return filmes.delete(id, db)
