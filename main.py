fb2307dc24501f3b5586157a3f4e470845cc2d6b
Luccas D T <luccasdt@gmail.com>
2022-03-31 20:10:41 -0300

    [DELETE] Comentário

--- main.py
+++ main.py
@@ -39,32 +39,3 @@ def delete_by_id(id : str):
     repository.delete_item(id)
     return repository.read_items()
     #Deve deletar um filme pelo id
-
-"""# Create -------------------------------------------------------
-@app.post("/filmes")
-def save_item(movie : Movie):
-    repository.save_item(movie)
-    #Deve criar um filme novo
-
-# Read ---------------------------------------------------------
-@app.get("/filmes")
-def read_items():
-    repository.read_items()
-    #Deve retornar todos os filmes cadastrados
-
-@app.get("/filmes/{id}")
-def read_by_id(id : int):
-    repository.read_by_id(id)
-    #Deve retornar um filme pelo id
-
-# Update -------------------------------------------------------
-@app.put("/filmes/{id}")
-def update_by_id(id: str, key : str, value):
-    repository.update_item(id, key, value)
-    #Deve atualizar um filme pelo id
-
-# Delete -------------------------------------------------------
-@app.delete("/filmes/{id}")
-def delete_by_id(id : str):
-    repository.delete_item(id)
-    #Deve deletar um filme pelo id"""
