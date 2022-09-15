27e44c206c82336e9a9e63483cd0e21cb176bb4e
LewisDamy <ldamy16@gmail.com>
2022-09-15 10:38:49 -0300

    fix host port

--- filmesAPI/filmesAPI/settings.py
+++ filmesAPI/filmesAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=7ao-!oqe2)vz4!+jgjvl@48tga^-0phub7w$zwz9r!yv_g@+5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
