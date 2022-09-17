c049d321c16605d0c21d1ae37516e17b2f85f6e6
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2022-09-17 20:36:20 -0300

    fix: adjusts names from filmes to movies

--- src/movies/infrastructure/containers.py
+++ src/movies/infrastructure/containers.py
@@ -3,9 +3,9 @@ from dependency_injector import containers, providers
 from config import Config
 
 
-from filmes.endpoints.filmes.repository import MoviesRepository
-from filmes.endpoints.filmes.service import MoviesService
-from filmes.infrastructure.database.database_sql import PostgresDatabase
+from movies.endpoints.movies.repository import MoviesRepository
+from movies.endpoints.movies.service import MoviesService
+from movies.infrastructure.database.database_sql import PostgresDatabase
 
 
 class Container(containers.DeclarativeContainer):
@@ -16,4 +16,4 @@ class Container(containers.DeclarativeContainer):
     movies_repository = providers.Factory(MoviesRepository, session_factory=postgres_db.provided.session)
 
     # services
-    users_services = providers.Factory(MoviesService, movies_repository=movies_repository,)
+    movies_services = providers.Factory(MoviesService, movies_repository=movies_repository)
