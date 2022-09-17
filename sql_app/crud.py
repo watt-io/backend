594b298f5706dadabbc9d1f7bcd4ced43975c921
Tiago Rosa <thenrique351@gmail.com>
2022-09-17 20:59:10 -0300

    Criacao dos metodos PUT e GETs por id e titulo

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -1,45 +1,23 @@
-from select import select
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from models import Movie
 
-class RepositorioFilmes():
-    def __init__(self, db:Session):
-        self.db = db
+class MovieRepository:
+ 
+    def find_all(db: Session) -> list[Movie]:
+        return db.query(Movie).all()
 
-    def inserir(self, filme:schemas.Movie):# filme[variavel]:[tipo]
-        db_movie = models.Movie(title=filme.title, 
-                                descreption=filme.description)
-        self.db.add(db_movie)
-        self.db.commit()
-        self.db.refresh(db_movie)
-        return db_movie
-
-    def consultar(self, movie_title: str):
-         #stmt = select(models.Movie).filter_by(title=movie_title)
-         #filme = self.db.execute(stmt).one()
-        #filme = self.db.query(models.Movie).filter(models.Movie.title == movie_title).first()
-        filme = self.db.query(models.Movie).filter(models.Movie.title == movie_title).first()
-        return filme
     
-    def exibir(self):
-        filmes = self.db.query(models.Movie).all()
-        return filmes
+    def save(db: Session, movie: Movie) -> Movie:
+        db.add(movie)
+        db.commit()
+        return movie
+
     
+    def find_by_title(db: Session, movie: Movie) -> Movie:
+        return db.query(Movie).filter(Movie.titulo == movie.titulo).first()
 
-
-
-
-def get_movie(db: Session, movie_title: str):
-    return db.query(models.Movie).filter(models.Movie.title == movie_title).first()
-
-def get_movies(db: Session):
-    return db.query(models.Movie).all()
-
-
-def create_movie(db: Session, movie: schemas.Movie):
-    db_movie = models.Movie(title=movie.title, description=movie.description)
-    db.add(db_movie)
-    db.commit()
-    db.refresh(db_movie)
-    return db_movie
+    
+    def find_by_id(db: Session, id: int) -> Movie:
+        return db.query(Movie).filter(Movie.id == id).first()
+    
