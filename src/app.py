a455cd97eed6a714f49691c0ed505373409b7d52
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2022-09-17 20:37:08 -0300

    feature: Config app fastapi

---
+++ src/app.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from httpx import HTTPStatusError
+
+from exception import ValidationException, RepositoryException
+from movies.middlewares.exception_handler import generic_request_exception_handler
+
+
+def create_app() -> False:
+    app = FastAPI()
+    from movies.infrastructure.containers import Container
+    container = Container()
+
+    from movies.endpoints.movies import controller as movies_module
+    movies_module.configure(app)
+
+    container.wire(modules=[movies_module])
+
+    app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'],
+                       allow_headers=['*'])
+
+    app.add_exception_handler(ValidationException, handler=generic_request_exception_handler)
+    app.add_exception_handler(RepositoryException, handler=generic_request_exception_handler)
+    app.add_exception_handler(HTTPStatusError, handler=generic_request_exception_handler)
+
+    return app
+
+
+api_app = create_app()
+
+if __name__ == '__main__':
+    uvicorn.run(api_app, host='0.0.0.0', port=8081)
