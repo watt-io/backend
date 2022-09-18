b215c473dbd23ce1ac81b6f1bcbf6d6c0df4203e
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2022-09-17 22:55:03 -0300

    fix: Adjusts endpoint change movies to filmes

--- src/movies/endpoints/movies/controller.py
+++ src/movies/endpoints/movies/controller.py
@@ -10,44 +10,44 @@ from movies.infrastructure.containers import Container
 router = APIRouter()
 
 
-@router.post("/movies", status_code=status.HTTP_201_CREATED, response_model=MoviesSchema)
+@router.post("/filmes", status_code=status.HTTP_201_CREATED, response_model=MoviesSchema)
 @inject
 async def create_movies(request: Request, register_movie: MovieSchema,
                         movies_service: MoviesService = Depends(Provide[Container.movies_services])):
     return await movies_service.create_movie(register_movie.dict())
 
 
-@router.get("/movies/{movie_id}", status_code=status.HTTP_200_OK, response_model=MoviesSchema, responses={
+@router.get("/filmes/{filme_id}", status_code=status.HTTP_200_OK, response_model=MoviesSchema, responses={
     404: {"description": "Movies not found"}
     })
 @inject
-async def get_by_id(request: Request, movie_id: int,
+async def get_by_id(request: Request, filme_id: int,
                     movies_service: MoviesService = Depends(Provide[Container.movies_services])):
-    return await movies_service.get_movie_by_id(movie_id=movie_id)
+    return await movies_service.get_movie_by_id(movie_id=filme_id)
 
 
-@router.get("/movies", status_code=status.HTTP_200_OK, response_model=List[MoviesSchema])
+@router.get("/filmes", status_code=status.HTTP_200_OK, response_model=List[MoviesSchema])
 @inject
 async def get_all_movies(request: Request, movies_service: MoviesService = Depends(Provide[Container.movies_services])):
     return await movies_service.get_all_movies()
 
 
-@router.put("/movies/{movie_id}", status_code=status.HTTP_204_NO_CONTENT, responses={
+@router.put("/filmes/{filme_id}", status_code=status.HTTP_204_NO_CONTENT, responses={
     404: {"description": "Movies not found"}
     })
 @inject
-async def update_movie(request: Request, movie_id: int, update_movie: UpdateMovieSchema,
+async def update_movie(request: Request, filme_id: int, update_movie: UpdateMovieSchema,
                        movies_service: MoviesService = Depends(Provide[Container.movies_services])):
-    return await movies_service.update_movie(movie_id=movie_id, movie_data=update_movie.dict(exclude_none=True))
+    return await movies_service.update_movie(movie_id=filme_id, movie_data=update_movie.dict(exclude_none=True))
 
 
-@router.delete("/movies/{movie_id}", status_code=status.HTTP_204_NO_CONTENT, responses={
+@router.delete("/filmes/{filme_id}", status_code=status.HTTP_204_NO_CONTENT, responses={
     404: {"description": "Movies not found"}
     })
 @inject
-async def delete_movie(request: Request, movie_id: int,
+async def delete_movie(request: Request, filme_id: int,
                        movies_service: MoviesService = Depends(Provide[Container.movies_services])):
-    return await movies_service.delete_movie(movie_id=movie_id)
+    return await movies_service.delete_movie(movie_id=filme_id)
 
 
 def configure(app: FastAPI):
