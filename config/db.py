dd0b4078f25585371dc0cae5cc7c825d224caf89
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-10-21 23:06:52 -0300

    Classe Model

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 
 engine = create_engine("sqlite:///./filme_database.db")
-
+meta = MetaData()
 conn = engine.connect()
---
+++ model/index.py
@@ -0,0 +1,3 @@
+from sqlalchemy import MetaData
+from model.filme import filmes
+meta = MetaData()
