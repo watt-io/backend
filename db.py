c795cc7fffdd757455094263be76d47f9d72e5e2
Joao Assis <joaovictorassis03@gmail.com>
2024-02-18 21:47:20 -0300

    [ADD]criando a classe Filme com os todos os metodos necessarios

--- db.py
+++ db.py
@@ -1,18 +1,8 @@
 import sqlalchemy
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 engine = sqlalchemy.create_engine('sqlite:///filmes.db')
 Base = declarative_base()
-
-class Filme(Base):
-    __tablename__ = 'filmes'
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=True)
-    titulo = Column(String(50), nullable=True)
-    ano = Column(Integer(), nullable=True)
-    genero = Column(String(15), nullable=True)
-
 
 Session = sessionmaker(bind=engine)
 
