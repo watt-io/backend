be7587a43ded500fd294a60575659cc4105f9225
Emanoel Sabino <105612311+emanoelsabino@users.noreply.github.com>
2024-03-03 00:36:27 -0300

    Add routers
    Configurando as rotas para executar o get e post dos filmes, executando o main.py já é possível testar os get e post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from app.routers import filmes_routers
 
 
 app = FastAPI()
@@ -8,6 +9,9 @@ app = FastAPI()
 def pagina_inicial():
     return {"Olá FastAPI": "Olá FastAPI"}
 
+
+# Configurando as rotas com o modulo filmes_routers
+app.include_router(filmes_routers.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
