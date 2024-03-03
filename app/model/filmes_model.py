dd3a7c9e58ba289caa300e99b7cc71d0fce56b20
Emanoel Sabino <105612311+emanoelsabino@users.noreply.github.com>
2024-03-03 00:22:50 -0300

    Add filmes_model
    Definindo o modelo de dados do filme

---
+++ app/model/filmes_model.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from app.db.database import *
+
+
+# Definindo o modelo de dados do filme
+class Filme(Base):
+    __tablename__ = "filmes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    lancamento = Column(Integer)
+
+# Criando as tabelas no banco de dados, se não existirem
+Base.metadata.create_all(bind=engine)
+
+# Criando classe de modelo para receber dados de entrada no POST
+class FilmeCreate(BaseModel):
+    titulo: str
+    lancamento: int
